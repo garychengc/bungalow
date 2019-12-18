b762b8f29801969e600b89d90b8f463be2eaacac
Gary Chen <garychen.whc@gmail.com>
2019-12-18 02:48:32 +0000

    set up basic server and client

--- server/routes/home.js
+++ server/routes/home.js
@@ -16,10 +16,8 @@ POST Request for the home page for geolocation of user
 */
 
 router.post("/", (req, res, next) => {
-  // const GOOGLE_API_KEY = "AIzaSyA09e-RrcHWfNSrsUtl-IWUKluHMxBRwvs";
   // const latitude = req.body.latitude;
   // const longitude = req.body.longitude;
-  // const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`;
 
   // axios.get(URL).then(response => {
   //   const address = response.data.results[0].formatted_address;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
